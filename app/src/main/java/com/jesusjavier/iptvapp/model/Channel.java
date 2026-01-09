c65952da098f6f38f09df3dbcb625948edcdcd31
jesssja <jessjabby1011@gmail.com>
2026-01-08 20:55:03 -0600

    Creado el modelo Channel.java para los canales

--- app/src/main/java/com/jesusjavier/iptvapp/model/Channel.java
+++ app/src/main/java/com/jesusjavier/iptvapp/model/Channel.java
@@ -1,4 +1,33 @@
 package com.jesusjavier.iptvapp.model;
 
 public class Channel {
+    private String nombre;
+    private String streamUrl;
+    private String logoUrl;
+    private String categoria;
+
+    // Constructor
+    public Channel(String nombre, String streamUrl, String logoUrl, String categoria) {
+        this.nombre = nombre;
+        this.streamUrl = streamUrl;
+        this.logoUrl = logoUrl;
+        this.categoria = categoria;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getStreamUrl() {
+        return streamUrl;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
 }
