ffa045a1fdf456bf54c543154f0e70a8015a6169
jesssja <jessjabby1011@gmail.com>
2026-01-09 20:36:28 -0600

    Creamos el ChannelAdapter.java para adapatar lo datos

--- app/src/main/java/com/jesusjavier/iptvapp/view/adapters/ChannelAdapter.java
+++ app/src/main/java/com/jesusjavier/iptvapp/view/adapters/ChannelAdapter.java
@@ -1,2 +1,52 @@
-package com.jesusjavier.iptvapp.view.adapters;public class ChannelAdapter {
+package com.jesusjavier.iptvapp.view.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jesusjavier.iptvapp.R;
+import com.jesusjavier.iptvapp.model.Channel;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.ChannelViewHolder> {
+    private List<Channel> channelList;
+
+    public ChannelAdapter(List<Channel> channelList) {
+        this.channelList = channelList;
+    }
+
+    @NonNull
+    @Override
+    public ChannelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_channel, parent, false);
+        return new ChannelViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChannelViewHolder holder, int position) {
+        Channel channel = channelList.get(position);
+        holder.txtName.setText(channel.getNombre());
+    }
+
+    @Override
+    public int getItemCount() {
+        return channelList.size();
+    }
+
+    public static class ChannelViewHolder extends RecyclerView.ViewHolder {
+        TextView txtName;
+        ImageView imgLogo;
+
+        public ChannelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.txt_channel_name);
+            imgLogo=itemView.findViewById(R.id.img_logo);
+        }
+    }
 }
